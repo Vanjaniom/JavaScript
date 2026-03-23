344b4f8bf1f349d7e83d33e6599a4839ef59e79c
Devloper Om <vanjaniom@gmail.com>
2026-03-23 20:08:44 +0530

    Custom Cursor Created

---
+++ Custom_Cursor/app.js
@@ -0,0 +1,6 @@
+let cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", function (e) {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
