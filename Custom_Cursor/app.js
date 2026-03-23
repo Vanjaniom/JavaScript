d0015fadc0f0d9dea3ebbd310587422165ff88c8
Devloper Om <vanjaniom@gmail.com>
2026-03-23 20:09:46 +0530

    Custom Cursor Updated

--- Custom_Cursor/app.js
+++ Custom_Cursor/app.js
@@ -1,6 +1,8 @@
 let cursor = document.querySelector(".cursor");
-
+let img = document.querySelector("img")
 document.addEventListener("mousemove", function (e) {
+  img.style.left = e.clientX + "px";
+  img.style.top = e.clientY + "px";
   cursor.style.left = e.clientX + "px";
   cursor.style.top = e.clientY + "px";
 });
